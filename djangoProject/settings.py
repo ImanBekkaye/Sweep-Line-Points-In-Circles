104c5361fd30ab3f6a8b8659a006e10cba0dd181
iman.bekkaye <imanbekkaye@hotmail.com>
2023-03-20 00:43:08 +0100

    static

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 STATIC_URL = 'front/build/static/'
 
-STATIC_ROOT = BASE_DIR / 'front'
+STATIC_ROOT = BASE_DIR / 'front/build'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'front/build/static'
