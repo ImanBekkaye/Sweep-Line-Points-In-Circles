32b7adce5a73c6639a0cfbaf553efc9cbef6ef68
iman.bekkaye <imanbekkaye@hotmail.com>
2023-03-20 00:26:19 +0100

    static

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -123,12 +123,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'front/build/static/'
+STATIC_URL = 'static/'
 
-STATIC_ROOT = BASE_DIR / 'front'
+STATIC_ROOT = BASE_DIR / 'front/build/public'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'front/build/static'
+    BASE_DIR / 'front/build/static',  BASE_DIR / '/static'
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
