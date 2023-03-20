da1f5a5b539ce8410bf1673e7c1b2e69c03f003a
iman.bekkaye <imanbekkaye@hotmail.com>
2023-03-20 09:27:39 +0100

    static

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -126,7 +126,8 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'front/build/static')
+    os.path.join(BASE_DIR, 'front/build/static'),
+    os.path.join(BASE_DIR, 'front/build')
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
